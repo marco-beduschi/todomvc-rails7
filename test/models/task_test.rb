1fec27afd955aa66d7d379c298c794d2e4982a5b
Marco Beduschi <marco.beduschi@nuuvem.com.br>
2023-06-22 09:21:40 -0300

    Add unit tests to Task

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'has a description' do
+    task = Task.new(description: 'Buy milk')
+
+    assert_equal 'Buy milk', task.description
+  end
+
+  test 'description is required' do
+    task = Task.new
+
+    assert_not task.valid?
+    assert_includes task.errors.full_messages, 'Description can’t be blank'
+  end
+
+  test 'has a completed status' do
+    task = Task.new(completed: true)
+
+    assert_equal true, task.completed
+  end
 end
